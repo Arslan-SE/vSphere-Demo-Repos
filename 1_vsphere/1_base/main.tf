2542bdfbb32b0592c2b1c95d6012fa430b228449
Arslan Saeed <arslan@hashicorp.com>
2019-01-31 05:42:18 -0500

    variablized module.

--- 1_vsphere/1_base/main.tf
+++ 1_vsphere/1_base/main.tf
@@ -1,15 +1,24 @@
 variable "prefix" { }
+variable "dc" { }
+variable "host" { }
+variable "cluster" { }
+variable "datastore" { }
+variable "vmtemplate" { }
+variable "vnet" { }
+variable "attDemoValue" { }
+variable "catEnvironment" { }
+
 
 module "base" {
   source = "ptfe.this-demo.rocks/PNC/base/vsphere"
   version = "1.0.7"
     prefix = "${var.prefix}"
-    dc = "PacketDatacenter"
-    cluster = "MainCluster"
-    host = ""
-    datastore = "datastore1"
-    vmtemplate = "UbuntuTemplate"
-    vnet = "VM Network"
-    attDemoValue = true
-    catEnvironment = "Production"
+    dc = "${var.dc}"
+    cluster = "${var.cluster}"
+    host = "${var.host}"
+    datastore = "${var.datastore}"
+    vmtemplate = "${var.vmtemplate}"
+    vnet = "${var.vnet}"
+    attDemoValue = "${var.attDemoValue}"
+    catEnvironment = "${var.catEnvironment}"
 }
