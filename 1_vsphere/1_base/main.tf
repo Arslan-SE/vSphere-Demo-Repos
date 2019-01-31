11bb71c119f0a8acfbed4525faa376b16c696467
Arslan Saeed <arslan@hashicorp.com>
2019-01-31 02:30:19 -0500

    vsphere only test

---
+++ 1_vsphere/1_base/main.tf
@@ -0,0 +1,12 @@
+module "test" {
+  source = "../modules/vsphere/base"
+    prefix = "AAR"
+    dc = "PacketDatacenter"
+    cluster = "MainCluster"
+    host = ""
+    datastore = "datastore1"
+    vmtemplate = "UbuntuTemplate"
+    vnet = "VM Network"
+    attDemoValue = true
+    catEnvironment = "Production"
+}
