c8389df48ca25788c47ce7e7584d29aaadc604bf
Arslan Saeed <arslan@hashicorp.com>
2019-01-31 08:54:18 -0500

    module update

--- 1_vsphere/2_network/main.tf
+++ 1_vsphere/2_network/main.tf
@@ -7,7 +7,7 @@ variable "workspaceName" { }
 # Networking
 module "networking" {
   source  = "ptfe.this-demo.rocks/PNC/networking/vsphere"
-  version = "1.0.1"
+  version = "1.0.2"
   org = "${var.org}"
   workspaceName = "${var.workspaceName}"
 
